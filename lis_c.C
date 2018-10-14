28bf6f23051c702a9967a327b4089eee21bf7a8c
Vineet Reddy <hotshot2797@gmail.com>
2018-10-14 22:42:35 +0530

    Calculate the execution time

--- lis_c.C
+++ lis_c.C
@@ -1,55 +1,63 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
+    clock_t begin = clock();
+
   //Initialsing my arrays
-    int ffff[argc];
-    int X[argc]; //Holds the int values of the CLI
+    int numArray[argc]; //Holds the int values of the CLI
     int P[argc];
     for (int i = 0; i<argc;i++){
-        P[i] = 0;
+        numArray[i] = 0;
     }
     int M[argc+1];
     for(int i = 0;i<=argc;i++){
         M[i] = 0;
     }
     int L = 0;
-    int lo,hi,newL,mid,k;
+    int lowVal,highVal,lPrime,middle,k;
 
 //This loop in incharge of converting the CLI to int
 for(int f = 1; f<argc; f++){ 
-    X[f] = atoi(argv[f]);
+    numArray[f] = atoi(argv[f]);
     // printf("The number entered: %d \n",X[f]);
 }
 //The Binary search happens here
     for(int i = 1; i<argc;i++){
-        lo = 1;
-        hi = L;
-        while(lo<=hi){
-            mid = floor((lo+hi)/2);
+        lowVal = 1;
+        highVal = L;
+        while(lowVal<=highVal){
+            middle = floor((lowVal+highVal)/2);
 
-            if(X[M[mid]]< X[i]){
-                lo = mid+1;
+            if(numArray[M[middle]]< numArray[i]){
+                lowVal = middle+1;
             }
             else{
-                hi = mid - 1;
+                highVal = middle - 1;
             }
         }
-        newL = lo;
-        P[i] = M[newL-1];
-        M[newL] = i;
-        if(newL > L){
-            L = newL;
+        lPrime = lowVal;
+        P[i] = M[lPrime-1];
+        M[lPrime] = i;
+        if(lPrime > L){
+            L = lPrime;
         }
     }
-    int S[100] = {0};
-    int S_rev[100] = {0};
+    int S[argc];
+    for (int i = 0; i<argc;i++){
+        S[i] = 0;
+    }
+    int S_rev[argc];
+    for (int i = 0; i<argc;i++){
+        S_rev[i] = 0;
+    }
     k = M[L];
 
     for(int j = L-1; j>-1;j--){
-        S[j]=X[k];
+        S[j]=numArray[k];
         k = P[k];    
         
         }
@@ -60,5 +68,8 @@ for(int f = 1; f<argc; f++){
     }
     printf("\n");
 
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Execution time: %f \n", time_spent);
     return 0;
 }
